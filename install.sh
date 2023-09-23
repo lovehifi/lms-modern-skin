c8dbfdecc1ec18f4de8a5bc8b460e7f5743a0bd5
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-23 13:11:59 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Check if any .tgz files exist in /root/
+if ls /root/*.tgz 1> /dev/null 2>&1; then
+  # Remove each .tgz file
+  for file in /root/*.tgz; do
+    rm -f "$file"
+  done
+fi
+
+wget https://raw.githubusercontent.com/lovehifi/lms-modern-skin/main/css.tgz
+
+wget https://raw.githubusercontent.com/lovehifi/lms-modern-skin/main/image.tgz
+
+tar -xzvf css.tgz -C /opt/logitechmediaserver-git/HTML/Default/
+
+tar -xzvf image.tgz -C /opt/logitechmediaserver-git/HTML/Default/html/images/
+
+systemctl restart logitechmediaserver-git.service
