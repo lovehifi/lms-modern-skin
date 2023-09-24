aca68e7741b564a8286c6e16f0490902cc84c607
lovehifi <81028393+lovehifi@users.noreply.github.com>
2023-09-24 22:13:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,11 @@ tar -xzvf css.tgz -C /opt/logitechmediaserver-git/HTML/Default/
 tar -xzvf image.tgz -C /opt/logitechmediaserver-git/HTML/Default/html/images/
 
 systemctl restart logitechmediaserver-git.service
+
+# Check if any .tgz files exist in /root/
+if ls /root/*.tgz 1> /dev/null 2>&1; then
+  # Remove each .tgz file
+  for file in /root/*.tgz; do
+    rm -f "$file"
+  done
+fi
